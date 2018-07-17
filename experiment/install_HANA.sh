4c75412df1b7de01ccabb07bcc27fe5a1f515be2
Page Bowers <pabowers@microsoft.com>
2018-07-17 18:30:22 +0000

    old variable still listed

--- experiment/install_HANA.sh
+++ experiment/install_HANA.sh
@@ -31,7 +31,7 @@ chmod 755 ./SAPCAR_LINUX.EXE
 ./SAPCAR_LINUX.EXE -manifest SIGNATURE.SMF -xvf IMDB_SERVER_LINUX.SAR
 
 # Generate the config and passwords
-awk -v sap_sid="$param_sap_sid" -v sap_instance_num="$param_sap_instancenum" -v sap_host_name="$sap_host_name" '{gsub("<SAP_SID>", sap_sid); gsub("<SAP_HOST_NAME>", sap_host_name); gsub("<SAP_INSTANCE_NUM>", sap_instance_num);}1' /tmp/sid_config_template.txt > ${SAP_SID}_configfile
+awk -v sap_sid="$param_sap_sid" -v sap_instance_num="$param_sap_instancenum" -v sap_host_name="$sap_host_name" '{gsub("<SAP_SID>", sap_sid); gsub("<SAP_HOST_NAME>", sap_host_name); gsub("<SAP_INSTANCE_NUM>", sap_instance_num);}1' /tmp/sid_config_template.txt > ${param_sap_sid}_configfile
 temp=`awk -v sap_adm_pw="$param_sap_sapadm" -v sid_adm_pw="$param_sap_sidadm" -v system_pw="$param_pw_db_system" '{gsub("<SAP_ADM_PW>", sap_adm_pw); gsub("<SID_ADM_PW>", sid_adm_pw); gsub("<SYSTEM_PW>", system_pw);}1' /tmp/sid_passwords_template.txt`
 # Pass the configs into the HANA install
 echo $temp | $hana_path/SAP_HANA_DATABASE/hdblcm --batch --action=install --configfile=${param_sap_sid}_configfile --read_password_from_stdin=xml

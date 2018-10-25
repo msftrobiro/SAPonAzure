d38a79e03e5405c93268db55fb68aa5da2440998
Page Bowers <40775967+pabowers@users.noreply.github.com>
2018-10-25 13:25:10 -0700

    Changed outputs to be more easily parsed (#83)

--- deploy/vm/modules/ha_pair/outputs.tf
+++ deploy/vm/modules/ha_pair/outputs.tf
@@ -1,15 +1,19 @@
 output "db0_ip" {
-  value = "Connect to db0 using ${var.vm_user}@${module.create_db0.fqdn}"
+  value = "${module.create_db0.fqdn}"
 }
 
 output "db1_ip" {
-  value = "Connect to db1 using ${var.vm_user}@${module.create_db1.fqdn}"
+  value = "${module.create_db1.fqdn}"
 }
 
-output "iscsi_ip" {
-  value = "Connect to iscsi using ${var.vm_user}@${module.nic_and_pip_setup_iscsi.fqdn}"
+output "db_vm_user" {
+  value = "${var.vm_user}"
 }
 
 output "windows_bastion_ip" {
   value = "${module.windows_bastion_host.ip}"
+}
+
+output "windows_bastion_user" {
+  value = "${var.bastion_username_windows}"
 }

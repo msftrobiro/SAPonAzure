d97a0df039cb31570b995dd3d73340171d29f932
Syeda Persia Aziz <syaziz@microsoft.com>
2019-06-14 10:12:40 -0700

    get rid of uninsall pip

--- monitor/configureMonitorVM.sh
+++ monitor/configureMonitorVM.sh
@@ -2,8 +2,6 @@
 
 #Updating apt-get
 apt-get -y update
-#uninstall pip first to avoid conflict with system pip while upgrading pip
-python3 -m pip uninstall pip
-apt install -y --reinstall python3-pip
+apt-get install -y python3-pip
 pip3 install --user --upgrade pip==19.1.1
 pip3 install pyhdb

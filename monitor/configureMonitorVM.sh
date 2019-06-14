61411d5d42fd3a6c5ad1cad2e02248e0007be2fb
Syeda Persia Aziz <syaziz@microsoft.com>
2019-06-14 10:01:01 -0700

    try with per user option

--- monitor/configureMonitorVM.sh
+++ monitor/configureMonitorVM.sh
@@ -5,5 +5,5 @@ apt-get -y update
 #uninstall pip first to avoid conflict with system pip while upgrading pip
 python3 -m pip uninstall pip
 apt install -y --reinstall python3-pip
-pip3 install --upgrade pip==19.1.1
+pip3 install --user --upgrade pip==19.1.1
 pip3 install pyhdb

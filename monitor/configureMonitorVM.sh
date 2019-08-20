034ee21cb67c24488c1b8bf8d03504c602d3c096
Syeda Persia Aziz <persia.aziz@yahoo.com>
2019-08-20 09:58:41 -0700

    Add retry with exponential backoff for monitor VM configuration (#206)
    * Add retry with exponential backoff for monitor VM configuration

--- monitor/configureMonitorVM.sh
+++ monitor/configureMonitorVM.sh
@@ -1,7 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-#Updating apt-get
-apt-get -y update
-apt-get install -y python3-pip
-python -m pip install -U pip
-pip3 install pyhdb
+# disable exit on error
+set +e
+
+ExecuteCommand() {
+    retryCounter=1
+    sleepSeconds=1
+    while [ $retryCounter -le 5 ]
+    do
+        echo "Try # $retryCounter: Command: $1"
+        #Sleep to allow the servers to recover in case the server was unable to serve the request
+        sleep $sleepSeconds
+        $1
+        if [ $? -eq 0 ]; then
+	        return
+        fi
+        retryCounter=$((retryCounter+1))
+        sleepSeconds=$((2*sleepSeconds))
+    done
+    >&2 echo "Error executing command: $1"
+    # Exit if all the retries failed
+    exit 1
+}
+
+# Update
+ExecuteCommand "apt-get -y update"
+# Install pip
+ExecuteCommand "apt-get install -y python3-pip"
+# Upgrade pip
+ExecuteCommand "python3 -m pip install -U pip"
+# Install pyhdb
+ExecuteCommand "pip3 install pyhdb"

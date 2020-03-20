651aedd7ac42e9d32ea7b7039dd713cecaeb9273
Nancy Chen <38501271+rukawata@users.noreply.github.com>
2020-03-19 19:31:32 -0700

    Fix wrong nic assignment (#344)
    Current NICs assignment for HANA server is incorrect (mentioned in PR #335, [comment](https://github.com/Azure/sap-hana/pull/335#issuecomment-600744551)).
    This PR fixes that.

--- deploy/v2/terraform/modules/hdb_node/main.tf
+++ deploy/v2/terraform/modules/hdb_node/main.tf
@@ -4,12 +4,12 @@
 
 # NETWORK SECURITY RULES =========================================================================================
 
-# Creates network security rule to deny external traffic for SAP admin subnet
-resource "azurerm_network_security_rule" "nsr-admin" {
+# Creates network security rule to deny external traffic for SAP db subnet
+resource "azurerm_network_security_rule" "nsr-db" {
   count                       = var.infrastructure.vnets.sap.subnet_admin.nsg.is_existing ? 0 : 1
   name                        = "deny-inbound-traffic"
-  resource_group_name         = var.nsg-admin[0].resource_group_name
-  network_security_group_name = var.nsg-admin[0].name
+  resource_group_name         = var.nsg-db[0].resource_group_name
+  network_security_group_name = var.nsg-db[0].name
   priority                    = 102
   direction                   = "Inbound"
   access                      = "deny"
@@ -20,12 +20,12 @@ resource "azurerm_network_security_rule" "nsr-admin" {
   destination_address_prefix  = var.infrastructure.vnets.sap.subnet_admin.prefix
 }
 
-# Creates network security rule for SAP DB subnet
-resource "azurerm_network_security_rule" "nsr-db" {
+# Creates network security rule for SAP admin subnet
+resource "azurerm_network_security_rule" "nsr-admin" {
   count                       = var.infrastructure.vnets.sap.subnet_db.nsg.is_existing ? 0 : 1
   name                        = "nsr-subnet-db"
-  resource_group_name         = var.nsg-db[0].resource_group_name
-  network_security_group_name = var.nsg-db[0].name
+  resource_group_name         = var.nsg-admin[0].resource_group_name
+  network_security_group_name = var.nsg-admin[0].name
   priority                    = 102
   direction                   = "Inbound"
   access                      = "allow"
@@ -175,8 +175,8 @@ resource "azurerm_linux_virtual_machine" "vm-dbnode" {
   resource_group_name = var.resource-group[0].name
   availability_set_id = azurerm_availability_set.hana-as[0].id
   network_interface_ids = [
-    azurerm_network_interface.nics-dbnodes-db[each.key].id,
-    azurerm_network_interface.nics-dbnodes-admin[each.key].id
+    azurerm_network_interface.nics-dbnodes-admin[each.key].id,
+    azurerm_network_interface.nics-dbnodes-db[each.key].id
   ]
   size                            = lookup(local.sizes, each.value.size).compute.vm_size
   admin_username                  = each.value.authentication.username

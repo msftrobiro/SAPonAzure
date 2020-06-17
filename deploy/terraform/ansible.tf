89018f91de34b210ea34ac01685b7dfec7c1ee05
Nancy Chen <38501271+rukawata@users.noreply.github.com>
2020-06-17 15:37:41 -0700

    Adjust dependency for null resource (#563)
    * change dependency and add missing connection

--- deploy/terraform/ansible.tf
+++ deploy/terraform/ansible.tf
@@ -3,8 +3,8 @@
     1. prepare-rti-files: copy required files for Ansible onto RTI
     2. ansible_playbook: run playbook
 +--------------------------------------4--------------------------------------*/
-resource "null_resource" "prepare-rti-files" {
-  depends_on = [module.hdb_node.dbnode-data-disk-att, module.jumpbox.prepare-rti, module.jumpbox.vm-windows]
+resource "null_resource" "prepare_rti_files" {
+  depends_on = [module.output_files.ansible-inventory, module.output_files.output-json, module.jumpbox.prepare-rti]
 
   triggers = {
     hosts  = sha1(local.file_hosts)
@@ -37,7 +37,16 @@ resource "null_resource" "prepare-rti-files" {
 
 resource "null_resource" "ansible_playbook" {
   count      = var.options.ansible_execution ? 1 : 0
-  depends_on = [null_resource.prepare-rti-files]
+  depends_on = [null_resource.prepare_rti_files, module.hdb_node.dbnode-data-disk-att, module.jumpbox.vm-windows]
+
+  connection {
+    type        = "ssh"
+    host        = module.jumpbox.rti-info.public_ip_address
+    user        = module.jumpbox.rti-info.authentication.username
+    private_key = module.jumpbox.rti-info.authentication.type == "key" ? file(var.sshkey.path_to_private_key) : null
+    password    = lookup(module.jumpbox.rti-info.authentication, "password", null)
+    timeout     = var.ssh-timeout
+  }
 
   # Run Ansible Playbook on jumpbox if ansible_execution set to true
   provisioner "remote-exec" {

dd55691b184d1cfa2d0e9ce1e7f27e93a479d2b8
Yunzi Zhang <62584551+YunziZhangAzure@users.noreply.github.com>
2020-07-27 22:11:39 -0700

    Hotfix: subnet will be deployed to corresponding vnet. (#663)
    Other resources eg: nsg will be deployed to the infra's RG

--- deploy/terraform/modules/app_tier/nsg-webdisp.tf
+++ deploy/terraform/modules/app_tier/nsg-webdisp.tf
@@ -2,8 +2,8 @@
 resource "azurerm_network_security_group" "nsg-web" {
   count               = local.enable_deployment && local.sub_web_defined ? (local.sub_web_nsg_exists ? 0 : 1) : 0
   name                = local.sub_web_nsg_name
-  location            = var.vnet-sap[0].location
-  resource_group_name = var.vnet-sap[0].resource_group_name
+  location            = var.resource-group[0].location
+  resource_group_name = var.resource-group[0].name
 }
 
 # Imports the SAP web subnet nsg data

3d399211aa72aa91a1c5b9af9482b10a587d72f7
Nancy Chen <38501271+rukawata@users.noreply.github.com>
2020-08-24 10:03:52 -0700

    Mask output date for pipeline (#703)
    set `sensitive = true`

--- deploy/terraform/run/sap_deployer/output.tf
+++ deploy/terraform/run/sap_deployer/output.tf
@@ -5,25 +5,31 @@ Description:
 */
 
 output "deployer_id" {
-  value = module.sap_deployer.deployer_id
+  sensitive = true
+  value     = module.sap_deployer.deployer_id
 }
 
 output "vnet_mgmt" {
-  value = module.sap_deployer.vnet_mgmt
+  sensitive = true
+  value     = module.sap_deployer.vnet_mgmt
 }
 
 output "subnet_mgmt" {
-  value = module.sap_deployer.subnet_mgmt
+  sensitive = true
+  value     = module.sap_deployer.subnet_mgmt
 }
 
 output "nsg_mgmt" {
-  value = module.sap_deployer.nsg_mgmt
+  sensitive = true
+  value     = module.sap_deployer.nsg_mgmt
 }
 
 output "deployer_uai" {
-  value = module.sap_deployer.deployer_uai
+  sensitive = true
+  value     = module.sap_deployer.deployer_uai
 }
 
 output "deployer" {
-  value = module.sap_deployer.deployers
+  sensitive = true
+  value     = module.sap_deployer.deployers
 }
--- deploy/terraform/run/sap_library/output.tf
+++ deploy/terraform/run/sap_library/output.tf
@@ -1,31 +1,39 @@
 output "tfstate_storage_account" {
-    value = module.sap_library.tfstate_storage_account
+  sensitive = true
+  value     = module.sap_library.tfstate_storage_account
 }
 
 output "sapbits_storage_account" {
-    value = module.sap_library.sapbits_storage_account
+  sensitive = true
+  value     = module.sap_library.sapbits_storage_account
 }
 
 output "storagecontainer_sapsystem" {
-    value = module.sap_library.storagecontainer_sapsystem
+  sensitive = true
+  value     = module.sap_library.storagecontainer_sapsystem
 }
 
 output "storagecontainer_saplandscape" {
-    value = module.sap_library.storagecontainer_saplandscape
+  sensitive = true
+  value     = module.sap_library.storagecontainer_saplandscape
 }
 
 output "storagecontainer_deployer" {
-    value = module.sap_library.storagecontainer_deployer
+  sensitive = true
+  value     = module.sap_library.storagecontainer_deployer
 }
 
 output "storagecontainer_saplibrary" {
-    value = module.sap_library.storagecontainer_saplibrary
+  sensitive = true
+  value     = module.sap_library.storagecontainer_saplibrary
 }
 
 output "storagecontainer_sapbits" {
-    value = module.sap_library.storagecontainer_sapbits
+  sensitive = true
+  value     = module.sap_library.storagecontainer_sapbits
 }
 
 output "fileshare_sapbits_name" {
-    value = module.sap_library.fileshare_sapbits_name
+  sensitive = true
+  value     = module.sap_library.fileshare_sapbits_name
 }

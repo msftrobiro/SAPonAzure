46df8275a01bac295edda42bd982a3d7b005ead8
rukawata <rukawata@gmail.com>
2020-08-24 14:38:14 -0700

    fix merge issue

--- deploy/terraform/bootstrap/sap_library/output.tf
+++ deploy/terraform/bootstrap/sap_library/output.tf
@@ -1,39 +1,24 @@
 output "tfstate_storage_account" {
   sensitive = true
-  value     = module.sap_library.tfstate_storage_account
+  value     = local.sa_tfstate
 }
 
 output "sapbits_storage_account" {
   sensitive = true
-  value     = module.sap_library.sapbits_storage_account
-}
-
-output "storagecontainer_sapsystem" {
-  sensitive = true
-  value     = module.sap_library.storagecontainer_sapsystem
-}
-
-output "storagecontainer_saplandscape" {
-  sensitive = true
-  value     = module.sap_library.storagecontainer_saplandscape
-}
-
-output "storagecontainer_deployer" {
-  sensitive = true
-  value     = module.sap_library.storagecontainer_deployer
+  value     = local.sa_sapbits
 }
 
 output "storagecontainer_saplibrary" {
   sensitive = true
-  value     = module.sap_library.storagecontainer_saplibrary
+  value     = local.storagecontainer_saplibrary
 }
 
 output "storagecontainer_sapbits" {
   sensitive = true
-  value     = module.sap_library.storagecontainer_sapbits
+  value     = local.storagecontainer_sapbits
 }
 
 output "fileshare_sapbits_name" {
   sensitive = true
-  value     = module.sap_library.fileshare_sapbits_name
+  value     = local.fileshare_sapbits_name
 }

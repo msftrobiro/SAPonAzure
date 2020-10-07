5a4620580ab41cb1b9264e6d25b5e6e64a7ccd9c
Nancy Chen <38501271+rukawata@users.noreply.github.com>
2020-10-07 11:46:13 -0700

    Add required output to saplib (#819)
    * add output to sap_library
    
    * Modify pipeline so the correct sap_library info is passed

--- deploy/terraform/terraform-units/modules/sap_library/output.tf
+++ deploy/terraform/terraform-units/modules/sap_library/output.tf
@@ -29,3 +29,15 @@ output "downloader_username_secret_name" {
 output "downloader_password_secret_name" {
   value = azurerm_key_vault_secret.downloader_password.name
 }
+
+output "remote_state_resource_group_name" {
+  value = local.rg_name
+}
+
+output "remote_state_storage_account_name" {
+  value = local.sa_tfstate_name
+}
+
+output "remote_state_container_name" {
+  value = local.sa_tfstate_container_name
+}

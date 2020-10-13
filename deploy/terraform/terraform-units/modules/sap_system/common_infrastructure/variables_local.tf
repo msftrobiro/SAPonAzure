30da30c868511fd7b075ebf3e206aa48af501ea2
Nancy Chen <38501271+rukawata@users.noreply.github.com>
2020-10-13 13:29:08 -0700

    add IMDS to working branch for Fe (#830)
    merge into feature/keyvault

--- deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/variables_local.tf
@@ -1,3 +1,9 @@
+/*
+Description:
+
+  Define local variables.
+*/
+
 variable "is_single_node_hana" {
   description = "Checks if single node hana architecture scenario is being deployed"
   default     = false

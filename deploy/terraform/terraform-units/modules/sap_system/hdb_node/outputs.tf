2480c795791377c65b6dcf2ab0739a6d685ca489
Yunzi Zhang <62584551+YunziZhangAzure@users.noreply.github.com>
2020-11-18 16:10:55 -0800

    remove  ansible_execution logic to avoid automatic execution of ansible (#932)

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/outputs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/outputs.tf
@@ -19,7 +19,3 @@ output "hana_database_info" {
   value = try(local.enable_deployment ? local.hana_database : map(false), {})
 }
 
-# Workaround to create dependency betweeen ../main.tf ansible_execution and module hdb_node
-output "dbnode_data_disk_att" {
-  value = azurerm_virtual_machine_data_disk_attachment.vm_dbnode_data_disk
-}

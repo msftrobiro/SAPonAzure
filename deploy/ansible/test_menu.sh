de6944beb06a6070e68d6736ae7450660118f5a3
Morgan Deegan <morgan.deegan@microsoft.com>
2021-01-26 12:18:09 -0800

    Update menu and add playbooks

--- deploy/ansible/test_menu.sh
+++ deploy/ansible/test_menu.sh
@@ -4,16 +4,18 @@ export           ANSIBLE_HOST_KEY_CHECKING=False
 
 PS3='Please select playbook: '
 options=(                           \
-        "00 playbook_00_transition_start_for_sap_install_refactor"                  \
-        "01 Base OS Config"         \
-        "02 SAP specific OS Config" \
-        "03 BOM Processing"         \
-        "04 SCS Install"            \
-        "05 HANA DB Install"        \
-        "06 DB Load"                \
-        "07 PAS Install"            \
-        "08 APP Install"            \
-        "09 WebDisp Install"        \
+        "Base OS Config"            \
+        "SAP specific OS Config"    \
+        "BOM Processing"            \
+        "SCS Install"               \
+        "HANA DB Install"           \
+        "DB Load"                   \
+        "PAS Install"               \
+        "APP Install"               \
+        "WebDisp Install"           \
+        "Pacemaker Setup"           \
+        "Pacemaker SCS Setup"       \
+        "Pacemaker HANA Setup"      \
         "Quit"                      \
 )
 
@@ -21,19 +23,22 @@ options=(                           \
 select opt in "${options[@]}";
 do
         echo "You selected ($REPLY) $opt";
-        case $REPLY in
-                1)      playbook=playbook_00_transition_start_for_sap_install_refactor.yaml;;
-                2)      playbook=playbook_01_os_base_config.yaml;;
-                3)      playbook=playbook_02_os_sap_specific_config.yaml;;
-                4)      playbook=playbook_03_bom_processing.yaml;;
-                5)      playbook=playbook_04a_sap_scs_install.yaml;;
-                6)      playbook=playbook_05a_hana_db_install.yaml;;
-                7)      playbook=playbook_06a_sap_dbload.yaml;;
-                8)      playbook=playbook_06b_sap_pas_install.yaml;;
-                9)      playbook=playbook_06c_sap_app_install.yaml;;
-                10)     playbook=playbook_06d_sap_web_install.yaml;;
-                11)     break;;
+        case $opt in
+                "Base OS Config")           playbook=playbook_01_os_base_config.yaml;;
+                "SAP specific OS Config")   playbook=playbook_02_os_sap_specific_config.yaml;;
+                "BOM Processing")           playbook=playbook_03_bom_processing.yaml;;
+                "SCS Install")              playbook=playbook_04a_sap_scs_install.yaml;;
+                "HANA DB Install")          playbook=playbook_05a_hana_db_install.yaml;;
+                "DB Load")                  playbook=playbook_06a_sap_dbload.yaml;;
+                "PAS Install")              playbook=playbook_06b_sap_pas_install.yaml;;
+                "APP Install")              playbook=playbook_06c_sap_app_install.yaml;;
+                "WebDisp Install")          playbook=playbook_06d_sap_web_install.yaml;;
+                "Pacemaker Setup")          playbook=playbook_07a_pacemaker.yaml;;
+                "Pacemaker SCS Setup")      playbook=playbook_07b_pacemaker_scs.yaml;;
+                "Pacemaker HANA Setup")     playbook=playbook_07c_pacemaker_hana.yaml;;
+                "Quit")                     break;;
         esac
+
         ansible-playbook                                                                                                \
           --inventory   new-hosts.yaml                                                                                  \
           --user        azureadm                                                                                        \

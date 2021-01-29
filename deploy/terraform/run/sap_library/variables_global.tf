66125f9b521704a18acf96bcb08effc3252d79bc
Kimmo Forss <kimmo.forss@microsoft.com>
2021-01-29 15:37:37 +0200

    Validation checks for SAP Library (#1034)
    * Validation checks for SAP Library
    
    * Updating comment

--- deploy/terraform/run/sap_library/variables_global.tf
+++ deploy/terraform/run/sap_library/variables_global.tf
@@ -1,7 +1,22 @@
 variable "infrastructure" {
-  description = "Details of the Azure infrastructure to deploy the SAP Library into"
+  description = "Details of the Azure infrastructure to deploy the SAP library into"
   default     = {}
+
+  validation {
+    condition = (
+      length(trimspace(try(var.infrastructure.region, ""))) != 0
+    )
+    error_message = "The region must be specified in the infrastructure.region field."
+  }
+
+  validation {
+    condition = (
+      length(trimspace(try(var.infrastructure.environment, ""))) != 0
+    )
+    error_message = "The environment must be specified in the infrastructure.environment field."
+  }
 }
+
 variable "storage_account_sapbits" {
   description = "Details of the Storage account for storing sap bits"
   default     = {}
@@ -17,6 +32,27 @@ variable "software" {
 variable "deployer" {
   description = "Details of deployer"
   default     = {}
+
+  validation {
+    condition = (
+      length(trimspace(try(var.deployer.region, ""))) != 0
+    )
+    error_message = "The region must be specified in the deployer.region field."
+  }
+
+  validation {
+    condition = (
+      length(trimspace(try(var.deployer.environment, ""))) != 0
+    )
+    error_message = "The environment must be specified in the deployer.environment field."
+  }
+
+  validation {
+    condition = (
+      length(trimspace(try(var.deployer.vnet, ""))) != 0
+    )
+    error_message = "The deployer VNet name must be specified in the deployer.vnet field."
+  }
 }
 
 variable "key_vault" {

51f7dc853e3a590c0de7a5f760c41ca97aa30e15
Kimmo Forss <kimmo.forss@microsoft.com>
2021-02-03 10:07:54 +0200

    [OUTPUTS] Adding a new Hosts YML file AB#140 (#1059)
    * Adding a new Hosts YML file
    
    * Added the missing parameters
    
    * Correct use of naming module
    
    * only define app server if more than one server
    
    * Don't return null if no app server

--- deploy/terraform/terraform-units/modules/sap_system/output_files/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/output_files/variables_local.tf
@@ -85,12 +85,12 @@ variable "tfstate_resource_id" {
   description = "Resource ID for tf state file"
 }
 
-variable "naming" {
-  description = "Defines the names for the resources"
-}
-
 variable "app_tier_os_types" {
   description = "Defines the app tier os types"
+}
+
+variable naming {
+  description = "Defines the names for the resources"
 }
 
 locals {

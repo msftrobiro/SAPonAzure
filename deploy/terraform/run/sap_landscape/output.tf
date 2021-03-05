d0a9e1c3a9ad60625e4be9099dc4abad85fefb23
Kimmo Forss <kimmo.forss@microsoft.com>
2021-03-05 13:19:04 +0200

    removed duplicate output

--- deploy/terraform/run/sap_landscape/output.tf
+++ deploy/terraform/run/sap_landscape/output.tf
@@ -61,6 +61,4 @@ output "witness_storage_account" {
 output "witness_storage_account_key" {
   value = module.sap_landscape.witness_storage_account_key
 }
-output "automation_version" {
-  value = local.version_label
-}
+
